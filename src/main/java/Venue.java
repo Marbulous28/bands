8b7daae25ca44dbc39c7a1c7f320dd87295adce7
Peter Beach <peterbeach2@gmail.com>
2016-05-16 08:34:29 -0700

    added back-end for venues.

---
+++ src/main/java/Venue.java
@@ -0,0 +1,58 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Venue {
+  private int id;
+  private String name;
+
+  public Venue(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Venue> all() {
+    String sql = "SELECT id, name FROM venues";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Venue.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherVenue) {
+    if (!(otherVenue instanceof Venue)) {
+      return false;
+    } else {
+      Venue newVenue = (Venue) otherVenue;
+      return this.getName().equals(newVenue.getName()) &&
+             this.getId() == newVenue.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO venues(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Venue find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM venues where id=:id";
+      Venue venue = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Venue.class);
+      return venue;
+    }
+  }
+
+}
--- src/test/java/VenueTest.java
+++ src/test/java/VenueTest.java
@@ -1,2 +1,58 @@
-// @Rule
-// public DatabaseRule database = new DatabaseRule();
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
+public class VenueTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Venue_instantiatesCorrectly_true() {
+    Venue newVenue = new Venue("Glastonburry");
+    assertEquals(true, newVenue instanceof Venue);
+  }
+
+  @Test
+  public void getName_categoryInstantiatesWithName_String() {
+    Venue newVenue = new Venue("Glastonburry");
+    assertEquals("Glastonburry", newVenue.getName());
+  }
+
+  @Test
+  public void all_emptyAtFirst_0() {
+    assertEquals(0, Venue.all().size());
+  }
+
+  @Test
+  public void equals_returnsTrueIfNamesAretheSame_true() {
+    Venue firstVenue = new Venue("Glastonburry");
+    Venue secondVenue = new Venue("Glastonburry");
+    assertTrue(firstVenue.equals(secondVenue));
+  }
+
+  @Test
+  public void save_savesObjectIntoDatabase_true() {
+    Venue newVenue = new Venue("Glastonburry");
+    newVenue.save();
+    assertTrue(Venue.all().get(0).equals(newVenue));
+  }
+
+  @Test
+  public void save_assignsIdToObject_int() {
+    Venue newVenue = new Venue("Glastonburry");
+    newVenue.save();
+    Venue savedVenue = Venue.all().get(0);
+    assertEquals(newVenue.getId(), savedVenue.getId());
+  }
+
+  @Test
+  public void find_findVenueInDatabase_true() {
+    Venue newVenue = new Venue("Glastonburry");
+    newVenue.save();
+    Venue savedVenue = Venue.find(newVenue.getId());
+    assertTrue(newVenue.equals(savedVenue));
+  }
+
+}
